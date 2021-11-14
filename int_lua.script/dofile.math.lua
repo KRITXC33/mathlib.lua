76920b9f03feebd7e3a50038b6498d84cf313b18
Kritic <79935756+Kritxc@users.noreply.github.com>
2021-11-14 14:46:55 -0800

    Add files via upload

---
+++ int_lua.script/dofile.math.lua
@@ -0,0 +1,5 @@
+#!/usr/local/mathlib/env lua
+
+dofile(math_eq.lua) --fetches script and runs
+
+--replace script in dofile with your own
---
+++ mathlib/math_eq.lua
@@ -0,0 +1,60 @@
+5 != 61
+--Simple addition equations for the interpereter to solve
+5 + 4
+10 + 6
+---[[
+    a = 4
+    b = 3
+    print(a*b)
+
+--]]
+
+math.type(3.0) --floater
+3 == 3.0
+math.type(4)
+4 != 7 --true equation
+
+--let's go and have the interpereter solve another problem
+
+---[[ --We made the comment into code again with a third dash
+a = 4
+b = 7
+c = 3
+print(a*b + c)
+--]]
+
+math.sin(3.14) --let's use sin this time
+math.type(60)
+60 == 60.0
+-- however...
+59 != 60
+--this is an easy equation, but we still see it is unequal
+
+--more complex equations
+
+a = 4
+b = 8
+c = 2
+d = 6
+print(a + c/b*d)
+
+
+a = 4
+b = 9
+c = 2
+d = 8
+print(a + d/b - c)
+
+--end of equations
+
+--let's alternate floaters and integers 
+
+math.type(4.0) --the floater has a decimal
+math.type(3)
+math.type(100.13)
+math.type(92) --the integer is a whole number 
+
+
+
+
+
